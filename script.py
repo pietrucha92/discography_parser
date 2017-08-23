2a25daaf46d08440aed18fb75e9c8855d3cf06f4
Piotr Jędrzejak <piotrek@MacBook-Air-Piotr.local>
2017-08-23 19:52:45 +0200

    Get discography by band (argv)

--- script.py
+++ script.py
@@ -1,11 +1,24 @@
 from bs4 import BeautifulSoup
 import urllib
+import sys
 
-source = urllib.urlopen('https://pl.wikipedia.org/wiki/Metallica').read()
+band = ""
+if len(sys.argv)==2:
+    band = sys.argv[1]
+    print ("Szukam danych dla zespolu: ", band)
+elif len(sys.argv)>2:
+    print "Podano za duzo danych"
+    exit
+else:
+    band = "Metallica"
+
+url = "https://pl.wikipedia.org/wiki/" + band
+print url
+source = urllib.urlopen(url).read()
 soup = BeautifulSoup(source, 'html.parser')
 heading = soup.find(id='Dyskografia')
 heading2 = heading.find_next('h3')
 titles = heading2.find_next('ul')
-discs = titles.find_all('a')
+discs = titles.find_all('li')
 for disc in discs:
-    print disc.string
+    print disc.get_text()
