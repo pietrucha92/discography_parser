e142d2950d013e6b9823457741402b8e54d9f455
Piotr Jędrzejak <piotrek@MacBook-Air-Piotr.local>
2017-08-23 19:25:07 +0200

    script dziala z Metallica

--- script.py
+++ script.py
@@ -3,7 +3,9 @@ import urllib
 
 source = urllib.urlopen('https://pl.wikipedia.org/wiki/Metallica').read()
 soup = BeautifulSoup(source, 'html.parser')
-heading = soup.find(id='Albumy_studyjne')
-discs = heading.find_next('ul')
+heading = soup.find(id='Dyskografia')
+heading2 = heading.find_next('h3')
+titles = heading2.find_next('ul')
+discs = titles.find_all('a')
 for disc in discs:
-    print disc
+    print disc.string
