b355007c5f8023a4452dbb8ca78ad7e390fd1865
Piotr Jędrzejak <piotrek@MacBook-Air-Piotr.local>
2017-08-24 18:25:38 +0200

    Discography from wiki with pol/eng language

--- script.py
+++ script.py
@@ -4,20 +4,27 @@ import urllib
 import sys
 
 band = ""
+lan= "en"
+discography = "Discography"
 if len(sys.argv)==2:
     band = sys.argv[1]
     print ("Szukam danych dla zespolu: ", band)
-elif len(sys.argv)>2:
+elif len(sys.argv)==3:
+    lan = sys.argv[1]
+    band = sys.argv[2]
+elif len(sys.argv)>3:
     print "Podano za duzo danych"
     exit
 else:
     band = "Metallica"
-
-url = "https://en.wikipedia.org/wiki/" + band
-print url
+if lan == "pl":
+    discography="Dyskografia"
+   
+url = "https://"+lan+".wikipedia.org/wiki/" + band
+print url, discography
 source = urllib.urlopen(url).read()
 soup = BeautifulSoup(source, 'html.parser')
-heading = soup.find(id='Discography')
+heading = soup.find(id=discography)
 titles = heading.find_next('ul')
 discs = titles.find_all('li')
 for disc in discs:
